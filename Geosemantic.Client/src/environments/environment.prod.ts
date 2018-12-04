ea97c0cafa6a1d3f4da19b1e08a13ea6ee6d22cb
Basheer Ahamed <basheerahamed.m@xenovex.com>
2018-12-04 23:39:18 +0530

    news feed link added

--- Geosemantic.Client/src/environments/environment.prod.ts
+++ Geosemantic.Client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     apiBaseUrl: 'http://yenbus.com:8003',
     production: false,
-    environment: 'Local',
+    environment: 'Prod',
     dateFormat: 'DD-MM-YYYY',
     dateTimeFormat: 'DD/MM/YYYY hh:mm:ss A',
     datePickerTheme: 'theme-blue',
-    showEnvironment: true,
+    showEnvironment: false,
     sessionIdleTimeOutSeconds: 1200,
     sessionWarningTimeOutSeconds: 20
   };
